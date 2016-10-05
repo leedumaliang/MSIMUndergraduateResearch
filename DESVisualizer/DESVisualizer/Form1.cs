a6ade20d88c38015f1cf8d37a5b212799cfd119c
leedumaliang <lduma002@odu.edu>
2016-10-05 11:56:04 -0400

    Added Arc and Event Capabilities

--- DESVisualizer/DESVisualizer/Form1.cs
+++ DESVisualizer/DESVisualizer/Form1.cs
@@ -16,19 +16,57 @@ namespace DESVisualizer
         {
             InitializeComponent();
             //DrawIt(0);
+            graphics = this.CreateGraphics();
+            this.Paint += new PaintEventHandler(pictureBox1_Paint); 
         }
+
+        private System.Drawing.Graphics graphics;
 
         private void DrawIt(int offset)
         {
-            System.Drawing.Graphics graphics = this.CreateGraphics();
             System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(
-                (50 * offset), 50, 50, 50);
+                (100 * offset), 50, 50, 50);
             graphics.DrawEllipse(System.Drawing.Pens.Black, rectangle);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             DrawIt(0); 
+            DrawIt(1); 
+            DrawIt(2); 
+            DrawIt(3); 
+        }
+        
+        private void drawArc(int current, int next)
+        {
+            Rectangle rect = new Rectangle(25 + (current * 100), 35 - (next * 10), 100 + (next * 100), 35 + (next * 10));
+            Pen blackPen = new Pen(Color.Black, 3);
+
+            // Create start and sweep angles on ellipse.
+            float startAngle = 0.0F;
+            float sweepAngle = -180.0F;
+
+            // Draw arc to screen.
+            graphics.DrawArc(blackPen, rect, startAngle, sweepAngle);
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            DrawIt(0);
+            DrawIt(1);
+            DrawIt(2);
+            DrawIt(3);
+            //drawArc(0);
+            drawArc(1, 0);
+            drawArc(0, 1);
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Form Load");
+
         }
     }
 }
